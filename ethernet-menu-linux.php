3b843011123c3057dfd6c1a0f1b617e931eea795
DanielSchmalen <daniel-schmalen@web.de>
2017-05-11 10:57:47 -0700

    Ethernet Menu Linux
    Final Version of my ethernet menu for Linux based OS. Traceroute has
    been exchanged with Telnet due to faulty libraries on the Zybo board
    during my project.

--- ethernet-menu-linux.php
+++ ethernet-menu-linux.php
@@ -31,7 +31,7 @@ while($exit === FALSE) {
 	$output;
 
 	echo "(1) - Ping \n";
-	echo "(2) - Traceroute \n";
+	echo "(2) - Telnet \n";
 	echo "(3) - DNS Lookup \n";
 	echo "(4) - Read Command History \n";
 	echo "(5) - Exit \n";
@@ -46,21 +46,52 @@ while($exit === FALSE) {
 	switch($input) {
 		case 1:
 			$parameter = readline("Destination IP: ");
-			$output = shell_exec('ping -c 5 ' . $parameter);
-			$history[$i] = 'ping -c 5 ' . $parameter;
-			echo $output;
+			
+			#	filter_var with the option FILTER_VALIDATE_IP checks the input on typical IP address properties
+			if(!filter_var($parameter, FILTER_VALIDATE_IP) === FALSE) {
+				$output = shell_exec('ping -c 5 ' . $parameter);
+				$history[$i] = 'ping -c 5 ' . $parameter;
+				echo $output;
+			}
+			else {
+			#	If the filter_var check fails and the input is not an IP address, the program will output the following.
+				$history[$i] = 'ping: Invalid IP address - ' . $parameter;
+				echo "Invalid IP address! \n";
+			}
 			break;
 		case 2:
-			$parameter = readline("Destination IP: ");
-			$output = shell_exec('traceroute ' . $parameter);
-			$history[$i] = 'traceroute ' . $parameter;
-			echo $output;
+			$parameter = readline("Destination IP and Port: ");
+			
+			#	explode will divide the entered IP address and Port into several values
+			$parameter = explode(" ",$parameter);
+			
+			#	In addition to the filter_var check, the program also checks if the given port is a numeric value.
+			if((!filter_var($parameter[0], FILTER_VALIDATE_IP) === FALSE) && (is_numeric($parameter[1]))) {
+				$output = shell_exec('telnet ' . $parameter[0] . ' ' . $parameter[1]);
+				$history[$i] = 'telnet ' . $parameter;
+				echo $output;
+			}
+			else {
+			#	If the filter_var check fails or the given port is not a numeric value, the program will output the following.
+				$history[$i] = 'telnet: Invalid IP address or port - ' . $parameter[0] . ' ' . $parameter[1];
+				echo "Invalid IP address or port! \n";
+			}
 			break;
 		case 3:
 			$parameter = readline("DNS Name: ");
-			$output = shell_exec('nslookup ' . $parameter);
-			$history[$i] = 'nslookup ' . $parameter;
-			echo $output;
+			
+			#	The user input has to be either a valid IP address or a valid DNS name. 
+			#	is_valid_domain_name is a self-written function at the end of the program to check the properties.
+			if((!filter_var($parameter, FILTER_VALIDATE_IP) === FALSE) || (is_valid_domain_name($parameter) == TRUE)) {
+				$output = shell_exec('nslookup ' . $parameter);
+				$history[$i] = 'nslookup ' . $parameter;
+				echo $output;
+			}
+			else {
+			#	If the filter_var check fails or the DNS name is not valid, the program will output the following.
+				$history[$i] = 'nslookup: Invalid IP address or DNS name - ' . $parameter;
+				echo "Invalid IP address or DNS name! \n";
+			}
 			break;
 		case 4:
 			foreach ( $history as $key=>$val ){
@@ -93,4 +124,10 @@ if($exit === TRUE) {
 }	
 else
 	echo "Oops, something went wrong! \n";
+
+function is_valid_domain_name($domain_name) {
+    return (preg_match("/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i", $domain_name) //valid chars check
+            && preg_match("/^.{1,253}$/", $domain_name) 									 //overall length check
+            && preg_match("/^[^\.]{1,63}(\.[^\.]{1,63})*$/", $domain_name)   ); 			 //length of each label
+}
 ?>
